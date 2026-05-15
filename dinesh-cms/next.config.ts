95467acf8331c3040b894d00f295c18f1c4c261a
rohithreddychappidi <rohithreddychappidi56@gmail.com>
2026-05-15 13:26:23 +0530

    Update next.config.ts

--- dinesh-cms/next.config.ts
+++ dinesh-cms/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   images: {
     localPatterns: [{ pathname: "/**" }],
@@ -9,5 +10,44 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "firebasestorage.googleapis.com" },
     ],
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "dineshkoyyalamudi.com",
+          },
+        ],
+        destination: "https://46dc.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.dineshkoyyalamudi.com",
+          },
+        ],
+        destination: "https://46dc.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.46dc.com",
+          },
+        ],
+        destination: "https://46dc.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
+
 export default nextConfig;
